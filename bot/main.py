fdd53c68e58fa71beba061ffafe3740dfc27e13c
X-Aleksey-X <TheHentWill@gmail.com>
2020-11-20 16:41:01 +0200

    PEP8 + обновил сообщение о запуске. Переместил в папку для бота

--- bot/main.py
+++ bot/main.py
@@ -2,14 +2,16 @@ import discord
 from discord.ext import commands
 import json
 
+
 with open("config.json", "r") as config_read:
     config = json.load(config_read)
 
 bot = commands.Bot(config['bot']['prefix'], case_insensitive = True, intents = discord.Intents.all())
 
+
 @bot.event
 async def on_ready():
-    print("Бот запущен!")
+    print(f"{bot.user} запущен, ID: {bot.user.id}")
 
 if __name__ == "__main__":
     bot.run(config['discord']['token'])
