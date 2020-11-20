1266bfd5e19fb1199273f672771b0f415887ae97
Михаил <barinovmishka@mail.ru>
2020-11-20 17:28:39 +0300

    Основа бота

---
+++ main.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import json
+
+with open("config.json", "r") as config_read:
+    config = json.load(config_read)
+
+bot = commands.Bot(config['bot']['prefix'], case_insensitive = True, intents = discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Бот запущен!")
+
+if __name__ == "__main__":
+    bot.run(config['discord']['token'])
