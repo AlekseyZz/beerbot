7818a042482442db706c9d38c0566a2752af8f2c
X-Aleksey-X <TheHentWill@gmail.com>
2020-12-03 18:31:39 +0200

    Ког для команд-утилит, есть команда для просмотра приглашений участника

---
+++ src/cogs/utilities.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+
+class Utilities(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command(name = "invites",
+                      aliases = ("check_invites", "invites_check", "приглашения"),
+                      brief = "Посмотреть приглашения пользователя и их статистику",
+                      usage = "invites <участник: ID, @упоминание, никнейм, никнейм#дискриминатор>")
+    async def user_invites(self, ctx: commands.Context, member: discord.Member = None) -> None:
+        """
+        Команда для просмотра приглашений пользователя: какие приглашения, их срок действия, сколько приглашено людей и прочие сведения.
+
+        :param ctx: Контекст в котором вызывается команда (подробнее читать https://discordpy.readthedocs.io/en/neo-docs/ext/commands/api.html?highlight=context#discord.ext.commands.Context)
+        :param member: Участник, приглашения которого надо посмотреть
+        :return: None
+        """
+
+        if member is None:
+            member = ctx.author
+
+        embed = discord.Embed()
+        embed.set_author(name = f"Приглашения {member.name}", icon_url = member.avatar_url)
+
+        for invite in await ctx.guild.invites():
+            if invite.inviter.id == member.id:
+                embed.add_field(name = f"Приглашение {invite.code}", value = f"""Канал: {invite.channel.mention}
+                                                                                Использований: {invite.uses}
+                                                                                Создано: {invite.created_at}
+                                                                                """, inline = False)
+
+        embed.set_footer(text = f"Команда {ctx.command.name} была выполнена успешно!")
+        await ctx.send(embed = embed)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Utilities(bot))
