dc13e7fd7f2315658c94c7f967c94b7e4fb58668
X-Aleksey-X <TheHentWill@gmail.com>
2020-12-03 18:30:27 +0200

    Ког для администраторских команд

---
+++ src/cogs/admin.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+from src.utils.errors import error_models
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    # TODO: Сделай администраторские команды
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Admin(bot))
