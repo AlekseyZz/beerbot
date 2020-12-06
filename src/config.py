3b2e5f761ad04bf7793140981fa3f1a88679f0ae
X-Aleksey-X <TheHentWill@gmail.com>
2020-12-06 14:15:07 +0200

    Добавил формат времени в конфиг + регионы дискорда

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from discord import Intents
 
 config = {
         "bot": {
-                "token": "token here",
+                "token": "generate token of bot on https:discord.com/developers",
                 "settings": {
                         "command_prefix": "$",
                         "case_insensitive": True,
@@ -20,6 +20,38 @@ config = {
                         }
                 },
                 "cogs": ["admin", "utilities"],
-                "cogs_path": "src.cogs"
+                "cogs_path": "cogs"
+        },
+        "discord": {
+                "regions": {
+                        "amsterdam": ":flag_nl: Амстердам",
+                        "brazil": ":flag_br: Бразилия",
+                        "dubai": ":flag_ae: Дубаи",
+                        "eu_central": ":flag_eu: Центральный Евросоюз",
+                        "eu_west": ":flag_eu: Западный Евросоюз",
+                        "europe": ":flag_eu: Европа",
+                        "frankfurt": ":flag_de: Франкфурт",
+                        "hongkong": ":flag_hk: Гонконг",
+                        "india": ":flag_in: Индия",
+                        "japan": ":flag_jp: Япония",
+                        "london": ":flag_gb: Лондон",
+                        "russia": ":flag_ru: Россия",
+                        "singapore": ":flag_sg: Сингапур",
+                        "southafrica": ":flag_za: Южная Африка",
+                        "south_korea": ":flag_hk: Южная Корея",
+                        "sydney": ":flag_au: Сидней",
+                        "us_central": ":flag_us: Центральный США",
+                        "us_east": ":flag_us: Восточный США",
+                        "us_south": ":flag_us: Южный США",
+                        "us_west": ":flag_us: Западный США",
+                        "vip_amsterdam": "VIP Амстердам",
+                        "vip_us_east": ":flag_us: VIP Восточный США",
+                        "vip_us_west": ":flag_us: VIP Западный США"
+                },
+                "interface": {
+                        "time_format": "%H:%M:%S %d.%m.%Y"
+                }
         }
 }
+
+
