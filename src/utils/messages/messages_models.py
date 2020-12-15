37f7d6f31ce4d40f092f639a3f82c203c43880b8
X-Aleksey-X <TheHentWill@gmail.com>
2020-12-15 10:33:50 +0200

    Добавил генерацию сообщений об ошибке

--- src/utils/messages/messages_models.py
+++ src/utils/messages/messages_models.py
@@ -4,8 +4,15 @@ from discord.ext import commands
 from src.config import config
 
 
-async def generate_message(status: str, ctx: commands.Context) -> discord.Embed:
+async def generate_message_success(status: str, ctx: commands.Context) -> discord.Embed:
     embed: discord.Embed = discord.Embed(color = config["bot"]["messages"][f"{status}"]["color"])
     embed.set_footer(text = f"Команда {ctx.command.name} была выполнена успешно!")
+
+    return embed
+
+
+async def generate_message_error(ctx: commands.Context) -> discord.Embed:
+    embed: discord.Embed = discord.Embed(color = config["bot"]["messages"]["error"]["color"])
+    embed.set_footer(text = f"При выполнении команды {ctx.command.name} возникла ошибка!")
 
     return embed
