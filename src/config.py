042673cef62f857aa50f11a0a68fd3098d42bb18
X-Aleksey-X <TheHentWill@gmail.com>
2020-12-20 20:54:16 +0200

    Обновил конфиг

--- src/config.py
+++ src/config.py
@@ -24,6 +24,42 @@ config = {
                 "cogs_path": "cogs"
         },
         "discord": {
+                "permissions": {
+                        "create_instant_invite": "Создавать приглашения",
+                        "add_reactions": "Добавлять реакции",
+                        "administrator": "Администратор",
+                        "attach_files": "Прикреплять файлы",
+                        "ban_members": "Блокировать участников",
+                        "change_nickname": "Изменять никнейм",
+                        "connect": "Подключаться",
+                        "deafen_members": "Отключать звук участникам",
+                        "embed_links": "Вставлять ссылки",
+                        "external_emojis": "Использовать эмодзи с других серверов",
+                        "kick_members": "Выгонять участников",
+                        "manage_channels": "Управлять каналами",
+                        "manage_emojis": "Управлять эмодзи",
+                        "manage_guild": "Управлять сервером",
+                        "manage_messages": "Управлять сообщениями",
+                        "manage_nicknames": "Управлять никнеймами",
+                        "manage_permissions": "Управлять ролями",
+                        "manage_roles": "Управлять ролями",
+                        "manage_webhooks": "Управлять вебхуками",
+                        "mention_everyone": "Упоминать @everyone, @here и все роли",
+                        "move_members": "Перемещать участников",
+                        "mute_members": "Отключать голос участникам",
+                        "priority_speaker": "Приоритетный голос",
+                        "read_message_history": "Читать историю сообщений",
+                        "read_messages": "Читать сообщения",
+                        "send_messages": "Отправлять сообщения",
+                        "send_tts_messages": "Отправлять tts-сообщения",
+                        "speak": "Говорить",
+                        "stream": "Видео",
+                        "use_external_emojis": "Использовать эмодзи с других серверов",
+                        "use_voice_activation": "Активация по голосу",
+                        "view_audit_log": "Просмотр журнала аудита",
+                        "view_channel": "Просматривать канал",
+                        "view_guild_insights": "Смотреть аналитику сервера"
+                },
                 "regions": {
                         discord.VoiceRegion("amsterdam"): "Амстердам :flag_nl:",
                         discord.VoiceRegion("brazil"): "Бразилия :flag_br:",
@@ -40,7 +76,8 @@ config = {
                         discord.VoiceRegion("sydney"): "Сидней :flag_au:",
                 },
                 "interface": {
-                        "time_format": "%H:%M:%S %d.%m.%Y",
+                        "time_local": "ru",
+                        "time_format": "%d.%m.%Y %H:%M:%S",
                         "enable_disable_icons": {
                                 True: "<:discord_on:763327382659661835>",
                                 False: "<:discord_off:763327382672113664>"
@@ -51,15 +88,6 @@ config = {
                         discord.ContentFilter(1): "Для участников без ролей",
                         discord.ContentFilter(2): "Для всех участников"
                 },
-
-                "status": {
-                        discord.Status("online"): "<:online:789773268873117717> В сети",
-                        discord.Status("offline"): "<:offline:789773269255454730> Оффлайн",
-                        discord.Status("dnd"): "<:dnd:789773269217443840> Не беспокоить",
-                        discord.Status("idle"): "<:idle:789773269242871818> Не активен",
-                        discord.Status("invisible"): "<:offline:789773269255454730> Невидимка"
-                },
-
                 "verification_level": {
                         discord.VerificationLevel(0): "(Пусто) Нету",
                         discord.VerificationLevel(1): "(Низкий) Подтверждённый по e-mail аккаунт",
@@ -70,6 +98,18 @@ config = {
                 "notifications_level": {
                         discord.NotificationLevel(0): "Все сообщения",
                         discord.NotificationLevel(1): "Только @упоминания"
+                },
+                "channel_type": {
+                        discord.ChannelType(0): "<:text_channel:715235724525437062>",
+                        discord.ChannelType(2): "<:voice_channel:764559925220343860>",
+                        discord.ChannelType(5): "<:announcements_channel:764559906241511434>"
+                },
+                "status": {
+                        discord.Status("online"): "<:online:789898968938709002> В сети",
+                        discord.Status("offline"): "<:offline:789898968737382441> не в сети",
+                        discord.Status("invisible"): "<:offline:789898968737382441> Невидимый",
+                        discord.Status("dnd"): "<:dnd:789898968883920927> Не беспокоить",
+                        discord.Status("idle"): "<:idle:789898968632524831> Не активен",
                 }
         }
 }
